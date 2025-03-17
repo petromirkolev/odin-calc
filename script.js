5faa21b22254ff85f5ac87f749d1018846f58d23
Petromir Kolev <petromirkolev@gmail.com>
2025-03-17 11:33:55 +0200

    Finish handle num input function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Global vars
 let result = 0;
-let waitForOperation = false;
+let waitForOperation = true;
+let firstNum = 0;
 let secondNum = 0;
 let operator;
 
@@ -24,21 +25,37 @@ function evalInput(input) {
   }
   // Check if it is a num or operator
   if (Number(!isNaN(input))) {
+    handleNumInput(input);
   }
 }
 
 // If the input is "equal"
 function getResult() {
   // document.getElementById('result').textContent = result;
-  document.getElementById('result').textContent = 'Result';
+  document.getElementById('result').textContent = result;
 }
 // If the input is "clear"
 function clearCalculator() {
   result = 0;
+  firstNum = 0;
+  secondNum = 0;
+  operator = undefined;
   document.getElementById('result').textContent = result;
 }
 // If the input is a number
-function handleNumInput() {}
+function handleNumInput(num) {
+  // If it is the first input of the user
+  if (waitForOperation) {
+    firstNum === 0 ? (firstNum = num) : (firstNum += num);
+    console.log('First num');
+    console.log(firstNum);
+    // If it is the secondary input of the user
+  } else {
+    secondNum === 0 ? (secondNum = num) : (secondNum += num);
+    console.log('Second num');
+    console.log(secondNum);
+  }
+}
 // If the input is an operator
 function handleOpInput() {}
 
